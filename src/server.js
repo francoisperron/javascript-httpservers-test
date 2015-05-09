7fccbacd8c0debb47409b484a6693cb7d20e0c2a
Francois Perron <fperron@gmail.com>
2015-05-08 20:02:56 -0400

    it returns 200

--- src/server.js
+++ src/server.js
@@ -1,15 +1,19 @@
 (function () {
     "use strict";
 
+    var httpServer = require("http-server");
+
+
     var Server = function () {
+        this.server = httpServer.createServer();
     };
     module.exports = Server;
 
     Server.prototype.start = function() {
-
+        this.server.listen(5000);
     };
 
     Server.prototype.stop = function() {
-
+        this.server.close();
     };
 }());
